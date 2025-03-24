fd92437282b0ae28ee8e6627f7cd9af15428cc41
MrXiaoFengFeng <95534581+MrXiaoFengFeng@users.noreply.github.com>
2025-03-24 18:35:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-print("Hello Word!！！！！")
-print("Hello Word2!！！！！")
-print("Hello Word3!！！！！")
-print("Hello Word4!！！！！")
-print("Hello Word5!！！！！")
-print("Hello Word6!！！！！")
-print("Hello Word7!！！！！")
-print("通了!！！！！")
+import allure
+
+# 使用 allure.attach 将每条打印信息附加到报告中
+allure.attach("Hello Word!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word2!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word3!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word4!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word5!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word6!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("Hello Word7!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
+allure.attach("通了!！！！！", name="打印信息", attachment_type=allure.attachment_type.TEXT)
